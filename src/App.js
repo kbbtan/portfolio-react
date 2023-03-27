021e0bcee69f56d6cc67c6ebe33e2a688920938b
Kian Boon <kianboon@yctan.com>
2023-03-27 00:49:01 -0400

    Added Navbar with working navigation using React Router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
+// This component acts as the wrapper component for all pages.
+// Includes navbar.
+import { Outlet, Link } from "react-router-dom";
+
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+import "./App.css";
+
 function App() {
   return (
-    <div className="App">
-      <p>早上好世界</p>
-    </div>
+    <>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand><Link to="/">Home</Link></Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link><Link to="/projects">Projects</Link></Nav.Link>
+            <Nav.Link><Link to="/contact">Contact</Link></Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+
+      <Outlet/>
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,10 +1,46 @@
+// Library Imports.
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+// Route Component Imports.
 import App from './App';
+import Home from './routes/Home';
+import Projects from './routes/Projects';
+import Contact from './routes/Contact';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+
+    // TODO: Make error page for 404 errors.
+    errorElement: null,
+
+    // TODO: Make pages render as children under the App page.
+    children: [
+      {
+        path: "/",
+        element: <Home/>
+      },
+      {
+        path: "projects",
+        element: <Projects/>
+      },
+      {
+        path: "contact",
+        element: <Contact/>
+      }
+    ]
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
